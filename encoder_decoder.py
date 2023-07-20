4e1d96b57b39623fc4fb22ad820bc6b625e50078
HeatherCasto <139958256+HeatherCasto@users.noreply.github.com>
2023-07-19 23:40:07 -0400

    Update encoder_decoder.py
    added decoder and tested

--- encoder_decoder.py
+++ encoder_decoder.py
@@ -1,18 +1,19 @@
-
 def encode(password):
     password_encoded = ""
     for i in password:
         encoded_num = str((int(i) + 3) % 10)
         password_encoded += encoded_num
+      
     return password_encoded
 
-
-def decode(password_encoded):
-    password = ""
-    for i in password_encoded:
-        decoded_num = str((int(i) - 3) % 10)
-        password += decoded_num
-    return password
+def decoder(password):
+  b=[]
+  for i in password:
+    c=(int(i)-3)%10
+    str(c)
+    b.append(c)
+  for j in b:
+    print(j,end="")
 
 
 def main():
@@ -29,11 +30,10 @@ def main():
             password_encoded = encode(password)
             print("Your password has been encoded and stored!")
         elif option == 2:
-            print("The encoded password is", password_encoded, "and the original password is", password, ".")
+             decoder(password_encoded)
         elif option == 3:
             break
 
 
 if __name__ == "__main__":
     main()
-
